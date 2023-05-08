587fb82f54e7209496f240a433413696a3dc6de8
carlodominic <suaybaguio.carlodominic@gmail.com>
2023-05-08 22:35:00 +0800

    asda

--- script.js
+++ script.js
@@ -21,20 +21,17 @@ function magnifyProductImage(imgID, zoom) {
     container.addEventListener("wheel", zoomMagnifier);
   
     function moveMagnifier(e) {
-        var pos, x, y;
-        e.preventDefault();
-        pos = getCursorPos(e);
-        x = pos.x;
-        y = pos.y;
-        if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
-        if (x < w / zoom) {x = w / zoom;}
-        if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-        if (y < h / zoom) {y = h / zoom;}
-        glass.style.left = (x - w) + "px";
-        glass.style.top = (y - h) + "px";
-        glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
-        glass.style.display = "block";
-    }
+    var pos, x, y;
+    e.preventDefault();
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+    glass.style.display = "block";
+}
+
   
     container.addEventListener("mouseleave", function() {
         glass.style.display = "none";
